9462af88e841875a32cc64102b82bddcdc9b2603
Kevin <148005493+romulus8@users.noreply.github.com>
2024-02-17 14:08:43 -0500

    removing duplicate dependency files

--- api/app.js
+++ api/app.js
@@ -7,6 +7,8 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const exam = require('./models/exam');
+const { updateSearchIndex } = require('./models/exam');
 
 var app = express();
 
@@ -35,5 +37,26 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//exam creation endpoint
+app.post("/api/exams",(req,res) =>{
+  const newExam = new exam ({
+    examID: req.body.examID,
+    patientID:req.body.patientID,
+    date:req.body.date,
+    keyFindings:req.body.keyFindings,
+    brixiaScore:req.body.brixiaScore,
+    imageURL:req.body.imageURL
+    });
+  newExam.save()
+    .then((savedExam) => {res.status(201).json({message:"Exam created",exam: savedExam})});
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({message: "Error creating Exam"});
+    });
+
+});
+  
+
 
 module.exports = app;
