c23304e638e20b9cc547d121030e061ba03d7170
Kevin <148005493+romulus8@users.noreply.github.com>
2024-02-20 08:43:01 -0500

    redesigned post request, was overly complex

--- api/app.js
+++ api/app.js
@@ -1,47 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-const examsRouter = require('./models/exam');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const connectDB = require('./db');//importing mongodb atlas connecter
+const express = require('express');
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const cors = require('cors');
 const port =5000;
-var app = express();
+const connectDB = require('./db');//importing mongodb atlas connecter
+const examModel = require("./models/examModel");
 
-app.use(logger('dev'));
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/exams',examsRouter);
+const app =express()
 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 connectDB();//connects to techdive database
 
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended:true,
+    }),
+);
+app.use("/api/exams",examModel);
 
+
+
+//port
 app.listen(port,()=>{
     console.log(`****Server is running on port: ${port}`);
 });
--- api/routes/exam.js
+++ api/routes/exam.js
@@ -1,33 +1,28 @@
-//exam creation endpoint
-const express=require('express');
-const Exam=require(/models/exam);
-const router=express.Router();
+let mongoose = require("mongoose"),
+    express = require("express");
+    router = express.Router();
+
+//exam model
+const ExamSchema=require("./models/examModel");
 
 //create new exam
-router.post("/api/exams",async (req,res) =>{
-    if(!req.body.examID || !req.body.patientID || !req.body.date || !req.body.keyFindings || !req.body.brixiaScore || !req.body.imageURL){
-        return res.status(400).json({error:"examID,patientID, date, keyFindings, brixiaScore and imageURL are required."});
-    }
-    try{
-        const newExam = new Exam 
-        ({
-            examID: req.body.examID,
-            patientID:req.body.patientID,
-            date:req.body.date,
-            keyFindings:req.body.keyFindings,
-            brixiaScore:req.body.brixiaScore,
-            imageURL:req.body.imageURL
-        });
-        await newExam.save();
-        res.status(201).json(newExam);
-       
-    }
-    catch(someError){
-       {
-        console.error(someError);
-        res.status(500).json({message: "Error creating Exam"});
-       }
-  
-    };
+router.route("/api/exams").post(async (req,res,next) =>
+{
+    await ExamSchema
+        .create(req.body)
+        .then((result) =>
+        {
+            res.json
+            ({
+                data: result,
+                message: "Exam succesfully created!",
+                status:200,
+            });
+        })
+    .catch((err) => {
+        return next(err);
+    });
 });
+           
+
     module.exports = router;
