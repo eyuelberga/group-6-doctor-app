69c6723e67593f37a3d6ae2b24490169d8002bcd
jeramaya monteiro <jeramayamonteiro@gmail.com>
2024-02-23 10:42:25 -0500

    deleteExam

--- api/controllers/exam-controller.js
+++ api/controllers/exam-controller.js
@@ -17,8 +17,19 @@ const allExams = async (req, res) => {
 };
 
 
+const deleteExam = async (req, res) => {
+  let exams = await ExamSchema.find({ examId: req.params.examId })
+  if (exams.length < 1) return res.status(404).json({ success: false, message: "Exam not found!" });
+ exams = await ExamSchema.deleteOne({ examId: req.params.examId })
+  return res.status(200).json({
+    success: true,
+    exam: exams[0]
+  });
+};
 
 module.exports = {
   examDetail,
-  allExams
+  allExams,
+  deleteExam
+  
 };
